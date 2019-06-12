0c306e809eb49b137a13c3f8d4c93dfaa80bd4e9
seredovdev <dseredov@yandex.ru>
2019-06-12 18:21:06 +0300

    server

--- app/server.js
+++ app/server.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+
+    console.log('Incoming connection...');
+
+    let urlObj = url.parse(req.url, true);
+    
+    if (urlObj.pathname === '/test' && urlObj.query.mess) {
+        let message = 'Hello, world!\n' +
+            'Method: ' + req.method + '\n' +
+            'Message: ' + urlObj.query.mess;
+
+        res.end(message);
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+
+}).listen({port: 8000});
+
+console.log('Server running at http://127.0.0.1:8000');
