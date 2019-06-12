a33cfcca8f4bac24afe4e048286e772f70998f21
seredovdev <dseredov@yandex.ru>
2019-06-13 00:55:24 +0300

    -

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
@@ -10,14 +11,32 @@ const server = http.createServer((req, res) => {
     let urlObj = url.parse(req.url, true);
     
     if (urlObj.pathname === '/test' && urlObj.query.mess) {
+
         let message = 'Hello, world!\n' +
             'Method: ' + req.method + '\n' +
             'Message: ' + urlObj.query.mess;
 
         res.end(message);
+
+    } else if (urlObj.pathname === '/get-json') {
+
+        fs.readFile('./node_libraries/json/ru.json', {encoding: 'utf-8'}, (err, info) => {
+            
+            if (err) {
+                console.log(err);
+                res.statusCode = 500;
+                res.end('Error');
+            }
+
+            res.end(info);
+        });
+
     } else {
+
         res.statusCode = 404;
+
         res.end('Page not found');
+
     }
 
 }).listen({port: 8000});
